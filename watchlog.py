1777c12a1663ccd38e1586029179bf0408c716fb
xmanrui <841206767@qq.com>
2017-07-01 14:35:46 +0800

    PEP8修改

--- watchlog.py
+++ watchlog.py
@@ -11,14 +11,14 @@ class LogFileEventHandler(FileSystemEventHandler):
         self.chunk_size = os.path.getsize(log_filename)
 
     def on_modified(self, event):
-        '''
+        """"
         一次修改log_filename文件会调用三次on_modified，每次的event.src_path都不一样，类似:
         ./log\mylog.log___jb_tmp___
         ./log\mylog.log___jb_old___
         ./log\mylog.log
         前两次会加上后缀，此时mylog.log不可访问，第三次才可以，所以要判断event.src_path和self.log_filename，
         不相等则退出本函数。
-        '''
+        """
 
         if os.path.abspath(event.src_path) != os.path.abspath(self.log_filename):
             return
